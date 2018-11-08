ba0411009f57ef845a75a575da25466bee2cf044
weruder <myncijub@gmail.com>
2018-11-07 23:20:12 -0600

    Added placeholder for event add button.

--- byteMeCalendar/app/src/main/java/com/example/eptay/byteMeCalendar/MainActivity.java
+++ byteMeCalendar/app/src/main/java/com/example/eptay/byteMeCalendar/MainActivity.java
@@ -5,23 +5,45 @@ import android.os.Bundle;
 import android.view.View;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.widget.CalendarView;
 
 public class MainActivity extends AppCompatActivity {
+    /* MEMBER VARIABLES */
+    CalendarView calendarView;
+    FloatingActionButton fab;
 
+    int m_month = 0;
+    int m_day = 0;
+    int m_year = 0;
+
+    /* METHODS */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FloatingActionButton fab = findViewById(R.id.floatingActionButton);
+        //Instantiate each widget on the layout
+        calendarView = findViewById(R.id.calendarView);
+        fab = findViewById(R.id.floatingActionButton);
 
+        //On changing the date, change the text to be new date
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView calendarView, int year, int month, int day) {
+                m_year = year;
+                m_month = month;
+                m_day = day;
+                setTitle("Date: " + (month + 1) + " / " + day + " / " + year);
+            }
+        });
+
+        //TODO: On clicking the action button, should change view to event adding form
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Month = " + (m_month + 1) + " Day = " + m_day + " Year = " + m_year, Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
     }
-
 }
