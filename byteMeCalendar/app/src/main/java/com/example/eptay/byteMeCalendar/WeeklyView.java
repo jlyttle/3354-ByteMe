6870d521b4eb9f678f47ca83df427fa322da6767
Shahzad-Ansari <34826823+Shahzad-Ansari@users.noreply.github.com>
2018-11-19 21:13:11 -0600

    Added a weekly view and edited the XML screen to display week day names Included a header that displays the current date.

---
+++ byteMeCalendar/app/src/main/java/com/example/eptay/byteMeCalendar/WeeklyView.java
@@ -0,0 +1,62 @@
+package com.example.eptay.byteMeCalendar;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TableLayout;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+
+
+public class WeeklyView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weekly_view);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        Calendar weekInfo = Calendar.getInstance();
+        int year = weekInfo.get(Calendar.YEAR);
+        int month = weekInfo.get(Calendar.MONTH);
+        int day = weekInfo.get(Calendar.DAY_OF_MONTH);
+        String week = month+"/"+day+"/"+year;
+        int currentDay = Calendar.DAY_OF_WEEK;
+
+        TableLayout table = (TableLayout)findViewById(R.id.AddEvent );
+        ListView list = (ListView)findViewById(R.id.WeekList);
+
+        ArrayList<String> weekNames = new ArrayList<>();
+        weekNames.add("Monday");
+        weekNames.add("Tuesday");
+        weekNames.add("Wednesday");
+        weekNames.add("Thursday");
+        weekNames.add("Friday");
+        weekNames.add("Saturday");
+        weekNames.add("Sunday");
+
+        ArrayAdapter adapter = new ArrayAdapter(this , android.R.layout.simple_list_item_1, weekNames);
+        list.setAdapter(adapter);
+
+        TextView weekHeader = findViewById(R.id.WeekName);
+        weekHeader.setText(week);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
