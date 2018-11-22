bac286cd7a0b5ae679d6c7f537810f65698af537
Shahzad-Ansari <34826823+Shahzad-Ansari@users.noreply.github.com>
2018-11-22 00:48:20 -0600

    displays all dates in week , ability to change weeks manually but now with swipe and only on create

--- byteMeCalendar/app/src/main/java/com/example/eptay/byteMeCalendar/WeeklyView.java
+++ byteMeCalendar/app/src/main/java/com/example/eptay/byteMeCalendar/WeeklyView.java
@@ -1,5 +1,6 @@
 package com.example.eptay.byteMeCalendar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -13,30 +14,90 @@ import android.widget.SimpleAdapter;
 import android.widget.TableLayout;
 import android.widget.TextView;
 
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.time.DayOfWeek;
+import java.time.temporal.WeekFields;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 
+
+
 public class WeeklyView extends AppCompatActivity {
+    FloatingActionButton swipe;
+    int swipeCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weekly_view);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        Calendar weekInfo = Calendar.getInstance();
-        int year = weekInfo.get(Calendar.YEAR);
-        int month = weekInfo.get(Calendar.MONTH)+1;
-        int day = weekInfo.get(Calendar.DAY_OF_MONTH);
-        String week = month+"/"+day+"/"+year;
-        int currentDay = Calendar.DAY_OF_WEEK;
+        DateFormat dateFormat = new SimpleDateFormat("dd-MMM-yyyy");
+        ArrayList<Day> weekList = new ArrayList<>();
 
-        TableLayout table = (TableLayout)findViewById(R.id.AddEvent );
+
+        Calendar calendar = Calendar.getInstance();
+        Date now = new Date();
+        calendar.setTime(now);
+
+        int dayOfWeek = (calendar.get(Calendar.DAY_OF_WEEK))-1;
+        int swipeDisplacement = swipeCount*7;
+        calendar.add(Calendar.DAY_OF_YEAR, -dayOfWeek);
+        calendar.add(Calendar.DAY_OF_YEAR, swipeDisplacement);
+        Date dateSunday = calendar.getTime();
+        calendar.add(Calendar.DAY_OF_YEAR, 1 );
+        Date dateMonday = calendar.getTime();
+        calendar.add(Calendar.DAY_OF_YEAR, 1);
+        Date dateTuesday = calendar.getTime();
+        calendar.add(Calendar.DAY_OF_YEAR, 1);
+        Date dateWedensday = calendar.getTime();
+        calendar.add(Calendar.DAY_OF_YEAR, 1);
+        Date dateThursday = calendar.getTime();
+        calendar.add(Calendar.DAY_OF_YEAR, 1);
+        Date dateFriday = calendar.getTime();
+        calendar.add(Calendar.DAY_OF_YEAR, 1);
+        Date dateSaturday = calendar.getTime();
+
+
+
+        String mondayString = dateFormat.format(dateMonday);
+        String tuesdayString = dateFormat.format(dateTuesday);
+        String wedensdayString = dateFormat.format(dateWedensday);
+        String thursdayString = dateFormat.format(dateThursday);
+        String fridayString = dateFormat.format(dateFriday);
+        String saturdayString = dateFormat.format(dateSaturday);
+        String sundayString = dateFormat.format(dateSunday);
+
+        Day Sunday = new Day("Sunday" ,sundayString , 2);
+        Day Monday = new Day("Monday" ,mondayString ,3);
+        Day Tuesday = new Day("Tuesday" ,tuesdayString , 8);
+        Day Wedensday = new Day("Wedensday" ,wedensdayString , 1);
+        Day Thursday = new Day("Thursday" ,thursdayString , 5);
+        Day Friday = new Day("Friday" ,fridayString , 9);
+        Day Saturday = new Day("Saturday" ,saturdayString , 4);
+
+        weekList.add(Sunday);
+        weekList.add(Monday);
+        weekList.add(Tuesday);
+        weekList.add(Wedensday);
+        weekList.add(Thursday);
+        weekList.add(Friday);
+        weekList.add(Saturday);
+
+
+
+
+
+
+        TableLayout table = (TableLayout)findViewById(R.id.NextWeek );
         ListView list = (ListView)findViewById(R.id.WeekList);
 
         /*
@@ -68,25 +129,27 @@ public class WeeklyView extends AppCompatActivity {
         //TextView weekHeader = findViewById(R.id.WeekName);
         //weekHeader.setText(week);
 
-        ArrayList<Day> weekList = new ArrayList<>();
-        Day Monday = new Day("Monday" ,"1/1/01" , 4);
-        Day Tuesday = new Day("Tuesday" ,"1/2/01" , 8);
-        Day Wedensday = new Day("Wedensday" ,"1/3/01" , 1);
-        Day Thursday = new Day("Thursday" ,"1/4/01" , 5);
-        Day Friday = new Day("Friday" ,"1/5/01" , 9);
-        Day Saturday = new Day("Saturday" ,"1/6/01" , 4);
-        Day Sunday = new Day("Sunday" ,"1/7/01" , 2);
 
-        weekList.add(Monday);
-        weekList.add(Tuesday);
-        weekList.add(Wedensday);
-        weekList.add(Thursday);
-        weekList.add(Friday);
-        weekList.add(Saturday);
-        weekList.add(Sunday);
+
+
 
         WeekListAdapter adapter = new WeekListAdapter(this, R.layout.weekly_view_layout,weekList);
         list.setAdapter(adapter);
+
+        swipe = findViewById(R.id.NextWeek);
+
+        swipe.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+              swipeCount++;
+              System.out.println(swipeCount);
+
+            }
+        });
+
+    }
+
+    public void generateDates(){
 
     }
 
