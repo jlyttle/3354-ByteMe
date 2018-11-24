f770ddd075ccf87886a436fc74c007d0ccb4dcfd
weruder <myncijub@gmail.com>
2018-11-24 15:02:44 -0600

    Added Day class, changed nav drawer items.

---
+++ byteMeCalendar/app/src/main/java/com/example/eptay/byteMeCalendar/Day.java
@@ -0,0 +1,41 @@
+package com.example.eptay.byteMeCalendar;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class Day {
+    public Day(int year, int month, int day) {
+        m_year = year;
+        m_month = month;
+        m_day = day;
+        Calendar calendar = new GregorianCalendar(year, month, day);
+        m_dayName = m_weekdays[calendar.get(Calendar.DAY_OF_WEEK)];
+    }
+
+    String[] m_weekdays = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+    private ArrayList<Event> m_events = new ArrayList<>();
+    private int m_year;
+    private int m_month;
+    private int m_day;
+    private String m_dayName;
+
+    public int getYear() { return m_year; }
+    public int getMonth() { return m_month; }
+    public int getDayNum() { return m_day; }
+    public String getDayName() { return m_dayName; }
+    public ArrayList<Event> getEvents() { return m_events; }
+
+    public void addEvent(Event event) { m_events.add(event); }
+    public void removeEvent(Event event) throws Exception {
+        if (m_events.contains(event))
+        {
+            m_events.remove(event);
+        }
+        else
+        {
+            //Could not find the event to remove
+            throw new Exception("Could not find event to remove: " + event);
+        }
+    }
+}
--- byteMeCalendar/app/src/main/java/com/example/eptay/byteMeCalendar/Event.java
+++ byteMeCalendar/app/src/main/java/com/example/eptay/byteMeCalendar/Event.java
@@ -1,21 +1,35 @@
 package com.example.eptay.byteMeCalendar;
 
+import java.util.ArrayList;
+
 public class Event {
-    String m_description;
-    int m_startingTime;
-    int m_endingTime;
-    EventCategory m_category;
-    boolean m_repeating;
+    private String m_name;
+    private String m_description;
+    private int m_startingTime;
+    private int m_endingTime;
+    private EventCategory m_category;
+    private boolean m_repeating;
 
-    String getDescription() { return m_description; }
-    int getStartingTime() { return m_startingTime; }
-    int getEndingTime() { return m_endingTime; }
-    EventCategory getCategory() { return m_category; }
+    public String getName() { return m_name; }
+    public String getDescription() { return m_description; }
+    public int getStartingTime() { return m_startingTime; }
+    public int getEndingTime() { return m_endingTime; }
+    public EventCategory getCategory() { return m_category; }
 
-    void setDescription(String description) { m_description = description; }
-    void setCategory(EventCategory category) { m_category = category; }
-    void setStartingTime(int startingTime) { m_startingTime = startingTime; }
-    void setEndingTime(int endingTime) { m_endingTime = endingTime; }
+    public void setName(String name) { m_name = name; }
+    public void setDescription(String description) { m_description = description; }
+    public void setCategory(EventCategory category) { m_category = category; }
+    public void setStartingTime(int startingTime) { m_startingTime = startingTime; }
+    public void setEndingTime(int endingTime) { m_endingTime = endingTime; }
+    public void deleteCategory(EventCategory category) {
+        ArrayList<Event> events = category.getEvents();
+        for (int i = 0; i < events.size(); ++i) {
+            events.set(i, null);
+        }
+    }
 
-
+    @Override
+    public String toString() {
+        return m_name + ": \"" + m_description + "\" " + m_startingTime + "-" + m_endingTime;
+    }
 }
