636e8e8817aedabc27642706d0c22bf5e2c99046
weruder <myncijub@gmail.com>
2018-11-25 23:46:43 -0600

    Added event cache class

--- byteMeCalendar/app/src/main/java/com/example/eptay/byteMeCalendar/Event.java
+++ byteMeCalendar/app/src/main/java/com/example/eptay/byteMeCalendar/Event.java
@@ -3,19 +3,29 @@ package com.example.eptay.byteMeCalendar;
 import java.util.ArrayList;
 
 public class Event {
+    public enum RepeatingType { NONE, DAILY, WEEKLY, MONTHLY, YEARLY }
+
     private String m_name;
     private String m_description;
     private int m_startingTime;
     private int m_endingTime;
     private EventCategory m_category;
-    private boolean m_repeating;
+    private boolean m_repeating = false;
+    private Day m_startingDay;
+    private Day m_endingDay;
+    private RepeatingType m_repeatingType;
 
+    public RepeatingType getRepeatingType() { return m_repeatingType; }
     public String getName() { return m_name; }
     public String getDescription() { return m_description; }
     public int getStartingTime() { return m_startingTime; }
     public int getEndingTime() { return m_endingTime; }
     public EventCategory getCategory() { return m_category; }
+    public boolean isRepeating() { return m_repeating; }
+    public Day getStartDay() { return m_startingDay; }
+    public Day getEndDay() { return m_endingDay; }
 
+    public void setRepeatingType(RepeatingType repeatingType) { m_repeatingType = repeatingType; }
     public void setName(String name) { m_name = name; }
     public void setDescription(String description) { m_description = description; }
     public void setCategory(EventCategory category) { m_category = category; }
--- byteMeCalendar/app/src/main/java/com/example/eptay/byteMeCalendar/MainActivity.java
+++ byteMeCalendar/app/src/main/java/com/example/eptay/byteMeCalendar/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.eptay.byteMeCalendar;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -14,6 +15,8 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.widget.CalendarView;
 
+import com.google.gson.Gson;
+
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
@@ -23,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
     private CalendarView m_calendarView;
     private FloatingActionButton m_fab;
     private Calendar m_calendar = new GregorianCalendar();
+    private SharedPreferences m_preferences;
+    private EventCache m_eventCache;
 
     int m_month = m_calendar.get(Calendar.MONTH);
     int m_day = m_calendar.get(Calendar.DAY_OF_MONTH);
@@ -33,18 +38,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nav_drawer);
-        m_drawerLayout = findViewById(R.id.drawer_layout);
+        m_preferences = getPreferences(MODE_PRIVATE);
 
+        //Retrieve event cache from shared preferences
+        Gson gson = new Gson();
+        String json = m_preferences.getString("EventCache", "");
+        m_eventCache = gson.fromJson(json, EventCache.class);
+
+        //Setup toolbar
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         ActionBar actionbar = getSupportActionBar();
         actionbar.setDisplayHomeAsUpEnabled(true);
         actionbar.setHomeAsUpIndicator(R.drawable.menu);
 
-
         //Instantiate each widget on the layout
         m_calendarView = findViewById(R.id.calendarView);
         m_fab = findViewById(R.id.floatingActionButton);
+        m_drawerLayout = findViewById(R.id.drawer_layout);
 
         //On changing the date, change the text to be new date
         m_calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
@@ -104,5 +115,15 @@ public class MainActivity extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        SharedPreferences.Editor prefsEditor = m_preferences.edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(m_eventCache);
+        prefsEditor.putString("EventCache", json);
+        prefsEditor.commit();
     }
 }
