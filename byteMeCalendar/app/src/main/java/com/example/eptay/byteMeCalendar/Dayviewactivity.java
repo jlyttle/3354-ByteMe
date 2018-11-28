49c35fc77fb5043272f412a92ec332e318d9a545
ChooJosh <joshchoo@outlook.com>
2018-11-27 22:55:54 -0600

    Updated times for day view

--- byteMeCalendar/app/src/main/java/com/example/eptay/byteMeCalendar/Dayviewactivity.java
+++ byteMeCalendar/app/src/main/java/com/example/eptay/byteMeCalendar/Dayviewactivity.java
@@ -13,7 +13,7 @@ import java.util.List;
 public class Dayviewactivity extends AppCompatActivity {
 
     private static final String TAG = "Dayviewactivity";
-    private static final int HOUR_HEIGHT = 60; //Each hour in the scroll view is 60dp
+    private static final int HOUR_HEIGHT = 61; //Each hour in the scroll view is 60dp
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,64 +26,10 @@ public class Dayviewactivity extends AppCompatActivity {
         Day day = new Day(GlobalCalendar.getYear(), GlobalCalendar.getMonth(), GlobalCalendar.getDayNum());
         EventCache eventCache = EventCache.getInstance();
         List<Event> eventArrayList = eventCache.get(day);
-        for (Event event: eventArrayList) {
+        for (Event event : eventArrayList) {
 
         }
 
-        Time twelveam = new Time("12am", "", "");
-        Time oneam = new Time("1am", "", "");
-        Time twoam = new Time("2am", "", "");
-        Time threeam = new Time("3am", "", "");
-        Time fouram = new Time("4am", "", "");
-        Time fiveam = new Time("5am", "", "");
-        Time sixam = new Time("6am", "", "");
-        Time sevenam = new Time("7am", "", "");
-        Time eightam = new Time("8pm", "", "");
-        Time nineam = new Time("9am", "", "");
-        Time tenam = new Time("10am", "", "");
-        Time elevenam = new Time("11am", "", "");
-        Time twelvepm = new Time("12pm", "", "");
-        Time onepm = new Time("1pm", "", "");
-        Time twopm = new Time("2pm", "", "");
-        Time threepm = new Time("3pm", "", "");
-        Time fourpm = new Time("4pm", "", "");
-        Time fivepm = new Time("5am", "", "");
-        Time sixpm = new Time("6am", "", "");
-        Time sevenpm = new Time("7am", "", "");
-        Time eightpm = new Time("8am", "", "");
-        Time ninepm = new Time("9am", "", "");
-        Time tenpm = new Time("10am", "", "");
-        Time elevenpm = new Time("11am", "", "");
-
-        //add hour objects into arraylist
-        ArrayList<Time> hourList = new ArrayList<>();
-        hourList.add(twelveam);
-        hourList.add(oneam);
-        hourList.add(twoam);
-        hourList.add(threeam);
-        hourList.add(fouram);
-        hourList.add(fiveam);
-        hourList.add(sixam);
-        hourList.add(sevenam);
-        hourList.add(eightam);
-        hourList.add(nineam);
-        hourList.add(tenam);
-        hourList.add(elevenam);
-        hourList.add(twelvepm);
-        hourList.add(onepm);
-        hourList.add(twopm);
-        hourList.add(threepm);
-        hourList.add(fourpm);
-        hourList.add(fivepm);
-        hourList.add(sixpm);
-        hourList.add(sevenpm);
-        hourList.add(eightpm);
-        hourList.add(ninepm);
-        hourList.add(tenpm);
-        hourList.add(elevenpm);
-
-        HourListAdapter adapter = new HourListAdapter(this, R.layout.adapter_view_layout, hourList);
-        mListView.setAdapter(adapter);
     }
 
     public double calculateHeightOfEvent(Event event) {
