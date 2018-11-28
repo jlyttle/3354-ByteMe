ea37427d55e5310dc30dd343c71c3654d10e7cd4
ChooJosh <joshchoo@outlook.com>
2018-11-27 23:04:32 -0600

    deleted dayviewactivity (listview)

--- byteMeCalendar/app/src/main/java/com/example/eptay/byteMeCalendar/scrollingdayview.java
+++ byteMeCalendar/app/src/main/java/com/example/eptay/byteMeCalendar/scrollingdayview.java
@@ -1,20 +1,45 @@
 package com.example.eptay.byteMeCalendar;
 
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.List;
+
 public class scrollingdayview extends AppCompatActivity {
+
+    private static final String TAG = "scrollingdayview";
+    private static final int HOUR_HEIGHT = 61; //Each hour in the scroll view is 60dp
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scrollingdayview);
+        Log.d(TAG, "onCreate: Started.");
 
-        TextView textView = findViewById(R.id.textView26);
-        //LinearLayout linearLayout = findViewById(R.id.linearLayout);
-        //linearLayout.addView(button);
+
+
+        Day day = new Day(GlobalCalendar.getYear(), GlobalCalendar.getMonth(), GlobalCalendar.getDayNum());
+        EventCache eventCache = EventCache.getInstance();
+        List<Event> eventArrayList = eventCache.get(day);
+        for (Event event : eventArrayList) {
+
+        }
+
+    }
+
+    public double calculateHeightOfEvent(Event event) {
+        int startHour = event.getStartingHour();
+        int startMinute = event.getStartingMinute();
+        int endHour = event.getEndingHour();
+        int endMinute = event.getEndingMinute();
+        double factor = (endHour - startHour) + ((endMinute - startMinute) / 60.0);
+
+        return (factor * HOUR_HEIGHT);
     }
 }
