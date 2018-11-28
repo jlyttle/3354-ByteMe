553a22f07253de4bbc359ea79dd2b6f0a9d9f6af
weruder <myncijub@gmail.com>
2018-11-27 22:38:08 -0600

    Added overlapping layout for day view

--- byteMeCalendar/app/src/main/java/com/example/eptay/byteMeCalendar/scrollingdayview.java
+++ byteMeCalendar/app/src/main/java/com/example/eptay/byteMeCalendar/scrollingdayview.java
@@ -2,6 +2,9 @@ package com.example.eptay.byteMeCalendar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class scrollingdayview extends AppCompatActivity {
 
@@ -9,5 +12,9 @@ public class scrollingdayview extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scrollingdayview);
+
+        TextView textView = findViewById(R.id.textView26);
+        //LinearLayout linearLayout = findViewById(R.id.linearLayout);
+        //linearLayout.addView(button);
     }
 }
