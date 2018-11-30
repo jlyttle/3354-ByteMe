610e53e27610b3b3c841f51f5c72044ba8092f9b
ernesto0 <30987669+ernesto0@users.noreply.github.com>
2018-11-29 22:45:43 -0600

    a

--- byteMeCalendar/app/src/main/java/com/example/eptay/byteMeCalendar/shareEvent.java
+++ byteMeCalendar/app/src/main/java/com/example/eptay/byteMeCalendar/shareEvent.java
@@ -5,17 +5,17 @@ import android.os.AsyncTask;
 
 import com.goebl.david.Webb;
 
-public class shareEvent extends AsyncTask <Void, Void, String>{
+public class shareEvent extends AsyncTask <String, Void, String>{
     public shareEvent(){
 
     }
 
     @Override
-    protected String doInBackground(Void... voids) {
+    protected String doInBackground(String... url) {
         Webb webb = Webb.create();
         webb.post("http://10.0.2.2:5000/send")
-                .param("msg", "hi")
-                .param("phoneNum", "+19564550732")
+                .param("msg", url[0])
+                .param("phoneNum", url[1])
                 .ensureSuccess()
                 .asVoid();
         return null;
