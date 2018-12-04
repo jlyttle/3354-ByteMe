022fa42663f949c69ced12670c32107268c5f521
ChooJosh <joshchoo@outlook.com>
2018-12-03 22:05:10 -0600

    added lines in manifest to successfully send message

--- byteMeCalendar/app/src/main/java/com/example/eptay/byteMeCalendar/scrollingdayview.java
+++ byteMeCalendar/app/src/main/java/com/example/eptay/byteMeCalendar/scrollingdayview.java
@@ -1,5 +1,6 @@
 package com.example.eptay.byteMeCalendar;
-
+//imports
+import android.annotation.SuppressLint;
 import android.provider.Settings;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
@@ -10,6 +11,8 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.ScrollView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.Toast;
 import java.text.DateFormatSymbols;
@@ -19,21 +22,33 @@ import org.w3c.dom.Text;
 
 import java.text.DateFormat;
 import java.util.Calendar;
+import java.util.Collections;
 import java.util.GregorianCalendar;
 import java.util.List;
 
-public class scrollingdayview extends AppCompatActivity{
+//scrollingdayview class
+public class scrollingdayview extends AppCompatActivity {
 
+
+    //variables
     private static final String TAG = "scrollingdayview";
     private static final int HOUR_HEIGHT = 61; //Each hour
     // in the scroll view is 61dp
     private ScrollView scrollView;
     private final int LEFT = 100;
     private final int RIGHT = 300;
+    private EventCache m_eventCache = EventCache.getInstance();
+    int currentYear = GlobalCalendar.getYear();
+    int currentMonth = GlobalCalendar.getMonth();
+    int currentDay = GlobalCalendar.getDayNum();
+    Day selectedDay = new Day(currentYear, currentMonth, currentDay);
+    String nameOfDay = selectedDay.getDayName();
 
     public String getMonth(int month) {
-        return new DateFormatSymbols().getMonths()[month-1];
+        return new DateFormatSymbols().getMonths()[month - 1];
     }
+
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,22 +60,21 @@ public class scrollingdayview extends AppCompatActivity{
         //TODO refactor
         //Calendar calendar = Calendar.getInstance();
         Day selectedDay = new Day(GlobalCalendar.getYear(), GlobalCalendar.getMonth(), GlobalCalendar.getDayNum());
-        int currentYear = GlobalCalendar.getYear();
-        int currentMonth = GlobalCalendar.getMonth();
-        int currentDay = GlobalCalendar.getDayNum();
-        String nameOfDay = selectedDay.getDayName();
         String currentDate = (nameOfDay + ", " + getMonth(currentMonth + 1) + " " + currentDay + ", " + currentYear);
         //String currentDate = DateFormat.getDateInstance(DateFormat.FULL).format(calendar.getTime());
         TextView textViewDate = findViewById(R.id.textViewDate);
         textViewDate.setText(currentDate);
 
 
+        List<Event> events = getOrderedEventList();
+        drawEvents(events);
+
         scrollView.setOnTouchListener(new OnSwipeTouchListener(scrollingdayview.this) {
             public void onSwipeRight() {
                 GlobalCalendar.setPrevDay();
                 Day prevDay = new Day(GlobalCalendar.getYear(), GlobalCalendar.getMonth(), GlobalCalendar.getDayNum());
                 List<Event> prevEvents = EventCache.getInstance().get(prevDay);
-                for (Event event: prevEvents) {
+                for (Event event : prevEvents) {
                     double height = calculateHeightOfEvent(event);
                     double topMargin = calculateTimeDifference(event);
 
@@ -75,11 +89,12 @@ public class scrollingdayview extends AppCompatActivity{
                 textViewDate.setText(currentDate);
                 Toast.makeText(scrollingdayview.this, "left", Toast.LENGTH_SHORT).show();
             }
+
             public void onSwipeLeft() {
                 GlobalCalendar.setNextDay();
                 Day nextDay = new Day(GlobalCalendar.getYear(), GlobalCalendar.getMonth(), GlobalCalendar.getDayNum());
-                List <Event> nextEvents = EventCache.getInstance().get(nextDay);
-                for (Event event: nextEvents) {
+                List<Event> nextEvents = EventCache.getInstance().get(nextDay);
+                for (Event event : nextEvents) {
                     double height = calculateHeightOfEvent(event);
                     double topMargin = calculateTimeDifference(event);
 
@@ -118,11 +133,76 @@ public class scrollingdayview extends AppCompatActivity{
         return (factor * HOUR_HEIGHT);
     }
 
+    private List<Event> getOrderedEventList() {
+        List<Event> events = m_eventCache.get(selectedDay);
+        Collections.sort(events);
+        return events;
+    }
+
     //method to determine difference from 12am to event start time
-    public double calculateTimeDifference(Event event){
+    public double calculateTimeDifference(Event event) {
         int startHour = event.getStartingHour();
         int startMinute = event.getStartingMin();
         double factor = (startMinute / 60) + startHour;
         return (factor * HOUR_HEIGHT);
+    }
+
+    private void drawEvents(List<Event> events) {
+        for (Event event : events) {
+            int index = 0;
+            int startHour = event.getStartingHour();
+            int startMin = event.getStartingMin();
+            boolean PM = false;
+            if (startHour > 11) {
+                PM = true;
+                if (startHour > 12) {
+                    startHour -= 12;
+                }
+            } else if (startHour == 0) {
+                startHour = 12;
+            }
+        }
+    /*Draws all events to the tablelayout
+    final int HEIGHT = 100;
+    final int TIME_WIDTH = 200;
+    final int TITLE_WIDTH = 500;
+
+        m_tableLayout.removeAllViews();
+        for (Event event: events) {
+        int index = 0;
+        int startHour = event.getStartingHour();
+        int startMin = event.getStartingMin();
+        boolean PM = false;
+        if (startHour > 11) {
+            PM = true;
+            if (startHour > 12) {
+                startHour -= 12;
+            }
+        }
+        else if (startHour == 0) {
+            startHour = 12;
+        }
+        String timeStr;
+        if (startMin < 10) {
+            timeStr = PM ? startHour + ":0" + startMin + " PM" : startHour + ":0" + startMin + " AM";
+        }
+        else {
+            timeStr = PM ? startHour + ":" + startMin + " PM" : startHour + ":" + startMin + " AM";
+        }
+        TextView time = new TextView(this);
+        time.setText(timeStr);
+        TextView title = new TextView(this);
+        title.setText(event.getName());
+        TextView eventID = new TextView(this);
+        eventID.setText(event.getID());
+        eventID.setVisibility(View.INVISIBLE);
+        TableRow row = new TableRow(this);
+        row.addView(time, TIME_WIDTH, HEIGHT);
+        row.addView(title, TITLE_WIDTH, HEIGHT);
+        row.addView(eventID);
+        m_tableLayout.addView(row, index);
+        registerForContextMenu(row);
+        ++index;
+    }*/
     }
 }
